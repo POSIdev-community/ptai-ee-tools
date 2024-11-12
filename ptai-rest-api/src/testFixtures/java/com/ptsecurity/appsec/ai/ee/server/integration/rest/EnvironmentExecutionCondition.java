a7e1e0cc448315c1f1c93d3abad9b9b501df0405
zhukov_an <alexey.n.zhukov@gmail.com>
2024-11-12 17:58:30 +0300

    Integration tests @Environment annotation disabledFor field added

--- ptai-rest-api/src/testFixtures/java/com/ptsecurity/appsec/ai/ee/server/integration/rest/EnvironmentExecutionCondition.java
+++ ptai-rest-api/src/testFixtures/java/com/ptsecurity/appsec/ai/ee/server/integration/rest/EnvironmentExecutionCondition.java
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.extension.ExecutionCondition;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.platform.commons.support.AnnotationSupport;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class EnvironmentExecutionCondition implements ExecutionCondition {
+    private static List<ScanBrief.ApiVersion> ALL = Arrays.asList(ScanBrief.ApiVersion.values());
+
     @Override
     public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context){
         ScanBrief.ApiVersion activeEnvironment = Connection.CONNECTION().getVersion();
@@ -18,8 +18,6 @@ public class EnvironmentExecutionCondition implements ExecutionCondition {
             return ConditionEvaluationResult.disabled("There is no active environment");
 
         Set<ScanBrief.ApiVersion> enabledEnvironments = getEnabledEnvironment(context);
-        // If test not annotated with PT AI version - allow its execution
-        if (enabledEnvironments.isEmpty()) return ConditionEvaluationResult.enabled("No environment restrictions");
         return enabledEnvironments.contains(activeEnvironment)
                 ? ConditionEvaluationResult.enabled("Active environment is enabled")
                 : ConditionEvaluationResult.disabled("Active environment is not enabled");
@@ -27,10 +25,22 @@ public class EnvironmentExecutionCondition implements ExecutionCondition {
 
     private Set<ScanBrief.ApiVersion> getEnabledEnvironment(ExtensionContext context) {
         Set<ScanBrief.ApiVersion> enabledEnvironments = new HashSet<>();
-        context.getElement()
-                .flatMap(element -> AnnotationSupport.findAnnotation(element, Environment.class)
-                .map(Environment::enabledFor))
-                .ifPresent(array -> enabledEnvironments.addAll(Arrays.asList(array)));
+        // Check if method is annotated with Environment limitations
+        Optional<Environment> environment = context.getElement()
+                .flatMap(element -> AnnotationSupport.findAnnotation(element, Environment.class));
+        if (environment.isPresent()) {
+            Optional<ScanBrief.ApiVersion[]> enabledFor = environment.map(Environment::enabledFor);
+            if (enabledFor.isPresent() && enabledFor.get().length > 0)
+                // Enabled environments are defined explicitly
+                enabledEnvironments.addAll(Arrays.asList(enabledFor.get()));
+            else
+                // No explicit definition, use all defined environments
+                enabledEnvironments.addAll(ALL);
+            Optional<ScanBrief.ApiVersion[]> disabledFor = environment.map(Environment::disabledFor);
+            disabledFor.ifPresent(apiVersions -> Arrays.asList(apiVersions).forEach(enabledEnvironments::remove));
+        } else
+            // Method is not annotated, no environments restrictions
+            enabledEnvironments.addAll(ALL);
         return enabledEnvironments;
     }
 }
