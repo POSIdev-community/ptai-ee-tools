a6c7ba80648395db441d57b984625546d92f0371
dlavrenov <dlavrenov@ptsecurity.com>
2024-11-15 20:22:44 +0300

    ASINT-2055: Rename default file name

--- ptai-jenkins-plugin/src/main/resources/com/ptsecurity/appsec/ai/ee/utils/ci/integration/plugin/jenkins/workmode/subjobs/export/RawJson/config.groovy
+++ ptai-jenkins-plugin/src/main/resources/com/ptsecurity/appsec/ai/ee/utils/ci/integration/plugin/jenkins/workmode/subjobs/export/RawJson/config.groovy
@@ -9,7 +9,7 @@ f.entry(
         title: Resources.i18n_ast_settings_mode_synchronous_subjob_export_rawjson_file_label(),
         field: 'fileName') {
     f.textbox(
-            default: 'report_name.json')
+            default: 'raw_report_name.json')
 }
 
 f.advanced() {
