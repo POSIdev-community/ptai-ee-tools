aa786c26f885180cc875092160c13b841c7ecdad
dlavrenov <dlavrenov@ptsecurity.com>
2024-12-10 01:07:58 +0300

    ASINT-2163: Refactor exception in jenkins

--- ptai-data-structures/src/main/java/com/ptsecurity/appsec/ai/ee/scan/errors/SettingsMustBeSetUpException.java
+++ ptai-data-structures/src/main/java/com/ptsecurity/appsec/ai/ee/scan/errors/SettingsMustBeSetUpException.java
@@ -1,4 +1,4 @@
-package com.ptsecurity.appsec.ai.ee.utils.ci.integration.plugin.jenkins.exceptions;
+package com.ptsecurity.appsec.ai.ee.scan.errors;
 
 public class SettingsMustBeSetUpException extends RuntimeException {
     public SettingsMustBeSetUpException(String message) {
