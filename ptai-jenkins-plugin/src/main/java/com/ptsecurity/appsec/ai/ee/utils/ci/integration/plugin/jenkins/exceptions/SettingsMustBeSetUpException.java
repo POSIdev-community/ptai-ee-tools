db1e3b65cfc1bb149f87fb3c08ff1372f9e7bac4
dlavrenov <dlavrenov@ptsecurity.com>
2024-12-10 00:41:53 +0300

    ASINT-2163: Checking scan settings for null in jenkins

---
+++ ptai-jenkins-plugin/src/main/java/com/ptsecurity/appsec/ai/ee/utils/ci/integration/plugin/jenkins/exceptions/SettingsMustBeSetUpException.java
@@ -0,0 +1,7 @@
+package com.ptsecurity.appsec.ai.ee.utils.ci.integration.plugin.jenkins.exceptions;
+
+public class SettingsMustBeSetUpException extends RuntimeException {
+    public SettingsMustBeSetUpException(String message) {
+        super(message);
+    }
+}
