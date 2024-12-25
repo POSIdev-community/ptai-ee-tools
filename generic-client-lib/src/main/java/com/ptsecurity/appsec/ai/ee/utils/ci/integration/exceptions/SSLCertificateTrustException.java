ed299dfa5cb4117e95e67ae2ba3b40e6b29aae20
dlavrenov <kingkekovd@gmail.com>
2024-12-25 03:02:40 +0300

    ASINT-2245: Add error handling for invalid certificate

---
+++ generic-client-lib/src/main/java/com/ptsecurity/appsec/ai/ee/utils/ci/integration/exceptions/SSLCertificateTrustException.java
@@ -0,0 +1,7 @@
+package com.ptsecurity.appsec.ai.ee.utils.ci.integration.exceptions;
+
+public class SSLCertificateTrustException extends RuntimeException {
+    public SSLCertificateTrustException(String message) {
+        super(message);
+    }
+}
