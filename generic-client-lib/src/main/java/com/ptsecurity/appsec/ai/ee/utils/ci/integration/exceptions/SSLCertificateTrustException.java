45a147821a1a85c8009be2a976b5eb0afc82150d
dlavrenov <kingkekovd@gmail.com>
2026-01-20 09:15:04 +0300

    feat: [ASINT-4225] remove unused files

--- generic-client-lib/src/main/java/com/ptsecurity/appsec/ai/ee/utils/ci/integration/exceptions/SSLCertificateTrustException.java
+++
@@ -1,7 +0,0 @@
-package com.ptsecurity.appsec.ai.ee.utils.ci.integration.exceptions;
-
-public class SSLCertificateTrustException extends RuntimeException {
-    public SSLCertificateTrustException(String message) {
-        super(message);
-    }
-}
