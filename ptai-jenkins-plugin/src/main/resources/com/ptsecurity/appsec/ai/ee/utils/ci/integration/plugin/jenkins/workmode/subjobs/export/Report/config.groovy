49c027d12acd716c0cf57e9f0cf95a0298205ab9
dlavrenov <kingkekovd@gmail.com>
2026-02-09 17:56:07 +0300

    fix: [ASINT-4339] add report locale to jenkins ui settings

--- ptai-jenkins-plugin/src/main/resources/com/ptsecurity/appsec/ai/ee/utils/ci/integration/plugin/jenkins/workmode/subjobs/export/Report/config.groovy
+++ ptai-jenkins-plugin/src/main/resources/com/ptsecurity/appsec/ai/ee/utils/ci/integration/plugin/jenkins/workmode/subjobs/export/Report/config.groovy
@@ -21,6 +21,12 @@ f.entry(
 }
 
 f.entry(
+        title: Resources.i18n_ast_settings_mode_synchronous_subjob_export_report_locale_label(),
+        field: 'locale') {
+    f.textbox()
+}
+
+f.entry(
         title: Resources.i18n_ast_settings_mode_synchronous_subjob_export_report_includedfd_label(),
         field: 'includeDfd',
         default: 'true') {
