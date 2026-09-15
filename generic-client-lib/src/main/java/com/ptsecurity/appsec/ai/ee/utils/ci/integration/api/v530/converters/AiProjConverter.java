d1db0c03a9503a04dacca7e2a4a82b8b96af58d3
KekovD <kingkekovd@gmail.com>
2026-09-16 00:03:35 +0300

    fix: [ASL2-5958] JSON policy overrides UseSecurityPolicies from aiproj

--- generic-client-lib/src/main/java/com/ptsecurity/appsec/ai/ee/utils/ci/integration/api/v530/converters/AiProjConverter.java
+++ generic-client-lib/src/main/java/com/ptsecurity/appsec/ai/ee/utils/ci/integration/api/v530/converters/AiProjConverter.java
@@ -615,7 +615,9 @@ public class AiProjConverter {
             final Policy[] policy,
             @NonNull final SecurityPoliciesModel model) {
         if (null == policy) {
-            model.setCheckSecurityPoliciesAccordance(settings.isUseSecurityPolicies());
+            if (settings.isUseSecurityPolicies()) {
+                model.setCheckSecurityPoliciesAccordance(true);
+            }
             return model;
         }
 
